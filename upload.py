8c28d5ab6055b6c3f615f63fd3d5117e7749a298
ayushuk <ayushshukla30339@gmail.com>
2024-04-28 09:38:58 -0400

    Add upload template script

---
+++ upload.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+import shutil
+import subprocess
+import sys
+
+BASE_PATH = 'pros-docs/v5/_static/branchline'
+DOCS_REPO_URL = 'https://github.com/purduesigbots/pros-docs.git'
+
+def upload(name: str, version: str, new_template: bool):
+    """
+
+    Uploads a template and updated registry files to docs.
+    This will upload the contents to https://pros.cs.purdue.edu/
+
+    TODO: error handling
+    
+    """
+    # update registry files
+    shutil.copyfile('pros-branchline/pros-branchline.json', BASE_PATH)
+    shutil.copyfile(f'pros-branchline/templates/{name}.json', f'{BASE_PATH}/templates')
+
+    # move zip
+    if new_template:
+        os.mkdir(f'{BASE_PATH}/{name}')
+    shutil.move(f'{name}@{version}.zip', f'{BASE_PATH}/{name}')
+
+    print('Finished upload')
+
+    # push changes
+    # TODO: make a PR or push to main?
+
+def main():
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 3:
+        print("error: msising arguments")
+        sys.exit(1)
+
+    # Retrieve command-line arguments
+    name = sys.argv[1]
+    version = sys.argv[2]
+
+    upload(name, version, False)
+
+if __name__ == "__main__":
+    main()
+
